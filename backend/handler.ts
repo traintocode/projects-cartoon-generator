218959f5820c6402ee8744d080213602f0383b1b
James Charlesworth <jcharlesworthuk@gmail.com>
2024-03-03 18:16:07 +0000

    Added solution for frontend and backend code

--- backend/handler.ts
+++ backend/handler.ts
@@ -1,21 +1,26 @@
-import { APIGatewayEvent, APIGatewayEventRequestContext, Context } from "aws-lambda";
+import { APIGatewayEvent } from "aws-lambda";
 import OpenAI from "openai";
 
-const env = <{ OPENAI_KEY: string }>process.env;
-
 type RequestBody = {
-
+    subject: string
 };
 
-export async function main(event: APIGatewayEvent, context: Context) {
+export async function main(event: APIGatewayEvent) {
     const body = <RequestBody>JSON.parse(event.body!);
+
+    const openai = new OpenAI({ apiKey: process.env['OPENAI_KEY'] });
+
+    const prompt = `Create a cartoon image of ${body.subject}`;
+
+    const gptResponse = await openai.images.generate({
+        prompt,
+        model: "dall-e-3",
+        response_format: "url"
+    });
+    const result = gptResponse.data[0].url;
 
     return {
         statusCode: 200,
-        headers: {
-            ["Access-Control-Allow-Origin"]: "http://localhost:5173"
-        },
-        body: "ok"
+        body: result
     };
 }
-
